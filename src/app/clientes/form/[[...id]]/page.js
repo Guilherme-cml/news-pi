52a3e70e6b405f0c0e3e010e9c9050b927382781
Guilherme-cml <gcml1144@gmail.com>
2024-11-21 17:35:56 -0300

    clientes sem validators

--- src/app/clientes/form/[[...id]]/page.js
+++ src/app/clientes/form/[[...id]]/page.js
@@ -1,59 +1,128 @@
-'use client';
+'use client'
 
-import { useEffect, useState } from "react";
-import Pagina from "@/app/components/Pagina";
-import { Formik, Form } from "formik";
-import * as Yup from "yup";
+import Nav from "@/components/Nav";
+import { Formik } from "formik";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { Button, Container, Form } from "react-bootstrap";
+import { FaCheck } from "react-icons/fa";
+import { MdOutlineArrowBack } from "react-icons/md";
+import { mask, unmask } from "remask";
+import { v4 } from "uuid";
 
-const ClienteValidator = Yup.object().shape({
-    nome: Yup.string().required("Nome é obrigatório"),
-});
+export default function Page({ params }) {
 
-export default function ClienteForm({ params }) {
-    const router = useRouter();
-    const [cliente, setCliente] = useState({ nome: '' });
+    const route = useRouter()
 
-    useEffect(() => {
-        if (params.id) {
-            const clientes = JSON.parse(localStorage.getItem('clientes')) || [];
-            const clienteEncontrado = clientes.find(c => c.id === params.id);
-            if (clienteEncontrado) {
-                setCliente(clienteEncontrado);
-            }
-        }
-    }, [params.id]);
+    const clientes = JSON.parse(localStorage.getItem('clientes')) || []
+    const dados = clientes.find(item => item.id == params.id)
+    const cliente = dados || { nome: '', email: '', telefone: '', data_nascimento: '', tipo_documento: '', documento: '' }
 
-    const salvarCliente = (values) => {
-        const clientes = JSON.parse(localStorage.getItem('clientes')) || [];
-        if (params.id) {
-            const index = clientes.findIndex(c => c.id === params.id);
-            clientes[index] = { ...clientes[index], ...values };
+    function salvar(dados) {
+
+        if (cliente.id) {
+            Object.assign(cliente, dados)
         } else {
-            values.id = Date.now();
-            clientes.push(values);
+            dados.id = v4()
+            clientes.push(dados)
         }
-        localStorage.setItem('clientes', JSON.stringify(clientes));
-        router.push('/clientes');
-    };
+
+        localStorage.setItem('clientes', JSON.stringify(clientes))
+        return route.push('/clientes')
+    }
 
     return (
-        <Pagina titulo={params.id ? "Editar Cliente" : "Novo Cliente"}>
+        <>
+            <Nav />
+            <Container>
+
             <Formik
                 initialValues={cliente}
-                validationSchema={ClienteValidator}
-                onSubmit={salvarCliente}
+                onSubmit={values => salvar(values)}
             >
-                {({ handleChange, handleSubmit }) => (
-                    <Form onSubmit={handleSubmit}>
-                        <div>
-                            <label>Nome</label>
-                            <input name="nome" onChange={handleChange} />
-                        </div>
-                        <button type="submit">Salvar</button>
-                    </Form>
-                )}
+                {({
+                    values,
+                    handleChange,
+                    handleSubmit,
+                    setFieldValue,
+                }) => {
+                    return (
+                        <Form>
+                            <Form.Group className="mb-3" controlId="nome">
+                                <Form.Label>Nome</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    name="nome"
+                                    value={values.nome}
+                                    onChange={handleChange('nome')}
+                                />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="tipo_documento">
+                                <Form.Label>Tipo de Documento</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    name="tipo_documento"
+                                    value={values.tipo_documento}
+                                    onChange={handleChange('tipo_documento')}
+                                />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="documento">
+                                <Form.Label>Documento</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    name="documento"
+                                    value={values.documento}
+                                    onChange={handleChange('documento')}
+                                />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="email">
+                                <Form.Label>E-mail</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    name="email"
+                                    value={values.email}
+                                    onChange={handleChange('email')}
+                                />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="telefone">
+                                <Form.Label>Telefone</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    name="telefone"
+                                    value={values.telefone}
+                                    onChange={(value)=>{
+                                        setFieldValue('telefone', mask(value.target.value, '(99) 99999-9999'))
+                                    }}
+                                />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="data_nascimento">
+                                <Form.Label>Dt. Nascimento</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    name="data_nascimento"
+                                    value={values.data_nascimento}
+                                    onChange={(value)=>{
+                                        setFieldValue('data_nascimento', mask(value.target.value, '99/99/9999'))
+                                    }}
+                                />
+                            </Form.Group>
+                            <div className="text-center">
+                                <Button onClick={handleSubmit} variant="success">
+                                    <FaCheck /> Salvar
+                                </Button>
+                                <Link
+                                    href="/clientes"
+                                    className="btn btn-danger ms-2"
+                                >
+                                    <MdOutlineArrowBack /> Voltar
+                                </Link>
+                            </div>
+                        </Form>
+                    )
+                }}
             </Formik>
-        </Pagina>
-    );
-} 
+            </Container>
+        </>
+    )
+}
--- src/app/produtos/detalhes/[[...id]]/page.js
+++ src/app/produtos/detalhes/[[...id]]/page.js
@@ -11,6 +11,7 @@ export default function ProdutoDetalhes({ params }) {
     const [produto, setProduto] = useState(null);
     const [comment, setComment] = useState('');
     const [comments, setComments] = useState([]);
+    const [message, setMessage] = useState(null);
     const [cartItems, setCartItems] = useState([]);
     const { id } = useParams();
     const savedComments = localStorage.getItem(`comments-${id}`)
@@ -67,7 +68,7 @@ export default function ProdutoDetalhes({ params }) {
         });
         localStorage.setItem(`comments-${id}`, JSON.stringify(updatedComments));
         setComments(updatedComments);
-        setEditingCommentId(null);
+        
       };
       const addToCart = (product) => {
         const currentCart = JSON.parse(localStorage.getItem('cart') || '[]');
